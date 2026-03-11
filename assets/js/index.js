3097e989649d7ec428dfd969b855a3a13718171b
Maksym <praniknumberone@gmail.com>
2026-03-12 00:42:09 +0100

    add CSS styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,32 @@
-
-let tasks = []; 
-const input = document.querySelector('#task-input');
-const addBtn = document.querySelector('#add-btn');
-const list = document.querySelector('#task-list');
-
+let tasks = [];
+const input = document.querySelector("#task-input");
+const addBtn = document.querySelector("#add-btn");
+const list = document.querySelector("#task-list");
 
 function renderTasks() {
-    
-    list.innerHTML = '';
+  list.innerHTML = "";
 
-   
-    tasks.forEach((task) => {
-        
-        const li = document.createElement('li');
-        li.textContent = task.text + " "; 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = '❌';
-        deleteBtn.addEventListener('click', () => {
-            tasks = tasks.filter((t) => t.id !== task.id)
-            renderTasks();
-        });
-        li.append(deleteBtn);
-        list.append(li);
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+    li.textContent = task.text + " ";
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+    deleteBtn.addEventListener("click", () => {
+      tasks = tasks.filter((t) => t.id !== task.id);
+      renderTasks();
     });
+    li.append(deleteBtn);
+    list.append(li);
+  });
 }
-addBtn.addEventListener('click', () => {
-    const text = input.value.trim(); 
-    if (text !== '') {
-        tasks.push({
-            id: Date.now(), 
-            text: text
-        });
-        input.value = ''; 
-        renderTasks();
-    }
+addBtn.addEventListener("click", () => {
+  const text = input.value.trim();
+  if (text !== "") {
+    tasks.push({
+      id: Date.now(),
+      text: text,
+    });
+    input.value = "";
+    renderTasks();
+  }
 });
